ff201368bdb7f43093a6ee48ccca14619e22e004
Sean Balbale <sean.balbale@gmail.com>
2026-04-25 13:24:07 -0400

    Add unit tests for GetInput class to validate input handling and error messages (#21)
    Refactor GetInput class to convert character input to uppercase for consistency

--- gameofgames/src/main/java/edu/trincoll/GetInput.java
+++ gameofgames/src/main/java/edu/trincoll/GetInput.java
@@ -60,7 +60,7 @@ public class GetInput
     {
         String chars = java.util.Arrays.toString(validChars);
         System.out.println("Enter one of these characters: "+chars);
-        String myChar = reader.next();
+        String myChar = reader.next().toUpperCase();
         while(true)
         {  
             if(myChar.length()==1){
@@ -70,7 +70,7 @@ public class GetInput
                 }
             }
             System.out.println("Error: enter one of these characters: "+chars);
-            myChar = reader.next();
+            myChar = reader.next().toUpperCase();
         }
     }
 }
